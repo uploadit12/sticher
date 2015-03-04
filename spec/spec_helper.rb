f1c5c43facf29df13ba95d6d1a4a07fd3ab8920d
Rohan Pujari <rohanpujaris@gmail.com>
2015-03-04 23:54:52 +0530

    rspec configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,9 @@ ActiveRecord::Base.establish_connection(
 load File.dirname(__FILE__) + '/support/schema.rb'
 load File.dirname(__FILE__) + '/support/stich.rb'
 load File.dirname(__FILE__) + '/support/unstich.rb'
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation # :progress, :html, :textmate
+end
